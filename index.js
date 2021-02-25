625cd9e12e444d6bf934d024b4c367ae5d8db552
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-25 16:13:54 +0400

    Description and navigation added

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const kickStarterUrl="https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
-const movieSalesUrl="https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
-const videoGameSales="https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+const kickStarterUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+const movieSalesUrl =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+const videoGameSales =
+  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
 
 const canvasHeight = 800;
 const canvasWidth = 800;
 
-var movieArr=[];
-var gameArr=[];
+var movieArr = [];
+var gameArr = [];
 
-d3.select("body").append("title").text("Treemap Diagram")
-.attr("id","title");
+d3.select("body").append("title").text("Treemap Diagram").attr("id", "title");
 
-var svg = d3.select("#main").append("svg");
+var svg = d3.select("#main").append("svg").attr("id", "canvas");
 
-fetch(kickStarterUrl)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data);
-        fetch(movieSalesUrl)
-            .then(response=>response.json())
-            .then(dataMovie=>{
-                console.log(dataMovie);
-            })
-    })
+fetch(videoGameSales)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
