7da9251b8b86b1bf69c6f99b0ab4161414595ba8
Farid Huseynov <faridhuseynov.eng@gmail.com>
2021-02-26 19:59:38 +0400

    Legend block properly set up and satisfying all test cases

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const kickStarterUrl =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
+"https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json";
 const movieSalesUrl =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
+"https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json";
 const videoGameSales =
-  "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
+"https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json";
 
-const height = 700;
+const height = 800;
 const width = 1300;
 
 var movieArr = [];
@@ -13,7 +13,6 @@ var gameArr = [];
 
 d3.select("body").append("title").text("Treemap Diagram").attr("id", "title");
 format = d3.format(",d");
-color = d3.scaleOrdinal(d3.schemeCategory10);
 
 const ratio = 1;
 
@@ -22,12 +21,17 @@ var svg = d3.select("#main").append("svg")
 .attr("height",height)
 .attr("width",width);
 
-// const div = d3.select("#main").append("div").attr("id","tooltip");
+const div = d3.select("#main").append("div").attr("id","tooltip");
 
 fetch(videoGameSales)
-  .then((response) => response.json())
-  .then((data) => {
-
+.then((response) => response.json())
+.then((data) => {
+  
+  const legendBlockValues = data.children.map(child=>child.name);
+  color = d3.scaleOrdinal(d3.schemeCategory10);
+  
+  color.domain(legendBlockValues);
+  
     const root = treemap(data);
     const leaf = svg.selectAll("g")
       .data(root.leaves())
@@ -46,7 +50,26 @@ fetch(videoGameSales)
       .attr("class","tile")
       .attr("data-name",d=>d.data.name)
       .attr("data-category",d=>d.data.category)
-      .attr("data-value",d=>d.data.value);
+      .attr("data-value",d=>d.data.value)
+      .on("mouseover",(event,d)=>{
+        div.transition()
+            .duration(500)
+            .style("opacity",1)
+            .attr("data-value",()=>{
+              var check = (Math.round(parseFloat(d.value)*100)/100).toFixed(2);
+              //  ).toFixed(2);
+              return d.value;
+            });
+        
+        div.html(d.data.name+"<br />"+d.data.category+"<br />"+d.data.value)
+            .style("left",(event.pageX-30)+"px")
+            .style("top",(event.pageY-60)+"px");
+      })
+      .on("mouseout",()=>{
+        div.transition()
+            .duration(500)
+            .style("opacity",0);
+      });
 
   leaf.append("text")
     .selectAll("tspan")
@@ -57,7 +80,37 @@ fetch(videoGameSales)
       .text(d => d);
 
 
+const legendBlockCanvas = d3.select("#main").append("svg")
+                            .attr("id","legend-canvas");
 
+
+const legendBlock = legendBlockCanvas.append("g")
+                    .attr("id","legend")
+                    .attr("transform","translate(0,0)");
+                    // .attr("transform",
+                    // "translate("+(width/3)+","
+                    // +(height-150)+")");
+
+
+ legendBlock
+            .selectAll("rect")
+            .data(legendBlockValues)
+            .enter()
+            .append("rect")            
+            .attr("height",22)
+            .attr("width",22)
+            .attr("x",(d,i)=>(Math.floor(i/4)*100))
+            .attr("y",(d,i)=>((i%4)*35))
+            .style("fill",d=>color(d))
+            .attr("class","legend-item");
+
+ legendBlock.selectAll("text").data(legendBlockValues)
+            .enter()
+            .append("text")
+            .text(d=>d)
+            .attr("x",(d,i)=>(Math.floor(i/4)*100+35))
+            .attr("y",(d,i)=>((i%4)*35+15))
+            .style("font-size","14px");
   });
 
 function treemap(data) {
@@ -67,7 +120,7 @@ function treemap(data) {
     .tile(d3.treemapSquarify.ratio(1))
     .size([width/ratio, height])
     .padding(1)
-    .round(true)(d3.hierarchy(data)
+    .round(false)(d3.hierarchy(data)
       .sum(d => d.value)
       .sort((a, b) => b.value - a.value)));
 } 
